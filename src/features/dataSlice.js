ea6c969f58ac40c8a1e78d7ab4d65cdc4abf1682
JDiaz <jdiaz_007@outlook.com>
2023-10-05 19:40:03 -0700

    created thunk in dataSlice.js

--- src/features/dataSlice.js
+++ src/features/dataSlice.js
@@ -20,6 +20,15 @@ export const dataSlice = createSlice({
 
 })
 
+export const fetchData = () => {
+    const dataThunk = async (dispatch, getState) => {
+        const data = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${getState().data.objectId}`)
+        const resData = await data.json()
+        dispatch(setData(resData))
+    }
+    return dataThunk
+}
+
 export const { setData, incrementId, decrementId, customId, clearData } = dataSlice.actions
 
 export default dataSlice.reducer
