efa1b0a58873deb04cca9157a2b8249c0b4bc149
JDiaz <jdiaz_007@outlook.com>
2023-10-05 19:21:20 -0700

    removed unnecessary Provider tag

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import App from './App';
 import { store } from './store'
 import { Provider } from 'react-redux'
 
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>      
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
